d3e1dcbc065c29ecab2689fc98cbc631c4a9e856
Wissem <w.kharrat@stud.uni-hannover.de>
2025-08-19 11:42:01 +0200

    use relative paths

--- tensorflow/lite/micro/examples/mobile_net_v1/tools/generate_cc_from_png_using_tf.py
+++ tensorflow/lite/micro/examples/mobile_net_v1/tools/generate_cc_from_png_using_tf.py
@@ -5,7 +5,7 @@ import numpy as np
 # Load the .png image
 # image = Image.open("/home/wissemkharrat/repos/tflite-micro/tensorflow/lite/micro/examples/mobile_net_v1/testdata/stopsign.png").convert("RGB")
 
-img = tf.keras.utils.load_img("/home/wissemkharrat/repos/tflite-micro/tensorflow/lite/micro/examples/mobile_net_v1/testdata/image.png", target_size=(224, 224))
+img = tf.keras.utils.load_img("tensorflow/lite/micro/examples/mobile_net_v1/testdata/image.png", target_size=(224, 224))
 # Resize the image to 224x224 (or the required size for your model)
 # image = image.resize((224, 224))
 test_image = np.expand_dims(img, axis=0).astype(np.float32)
@@ -21,7 +21,7 @@ image_array = test_image.flatten()
 print(image_array)
 print(len(image_array))
 
-with open("/home/wissemkharrat/repos/tflite-micro/tensorflow/lite/micro/examples/mobile_net_v1/input_data.cc", "w") as f:
+with open("tensorflow/lite/micro/examples/mobile_net_v1/input_data.cc", "w") as f:
     f.write("#include \"input_data.h\"\n\n")
     f.write("alignas(16) const float g_image_array[] = {")
     for i in range(len(image_array)):
@@ -31,7 +31,7 @@ with open("/home/wissemkharrat/repos/tflite-micro/tensorflow/lite/micro/examples
             f.write(f"{image_array[i]}, ")
     f.write("};\n")
 
-with open("/home/wissemkharrat/repos/tflite-micro/tensorflow/lite/micro/examples/mobile_net_v1/input_data.h", "w") as f:
+with open("tensorflow/lite/micro/examples/mobile_net_v1/input_data.h", "w") as f:
     f.write("#ifndef INPUT_DATA_H_\n")
     f.write("#define INPUT_DATA_H_\n\n")
     f.write("extern const float g_image_array[];\n\n")
